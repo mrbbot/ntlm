63faa80efe9058d41c098a169ad7763a0781eb04
MrBBot <me@mrbbot.co.uk>
2019-01-31 08:20:13 +0000

    Added support for other HTTP methods and custom client

--- lib/src/http.dart
+++ lib/src/http.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 import 'dart:io';
 import 'dart:async';
 import 'package:ntlm/src/messages/messages.dart';
+
+typedef _RequestCallback = Future<Response> Function(
+  Map<String, String> ntlmHeaders,
+);
 
 class NTLMClient {
   String domain;
@@ -11,6 +17,7 @@ class NTLMClient {
   String password;
   String lmPassword;
   String ntPassword;
+  Client _inner;
 
   NTLMClient({
     this.domain = "",
@@ -19,6 +26,7 @@ class NTLMClient {
     this.password = null,
     this.lmPassword = null,
     this.ntPassword = null,
+    Client inner,
   }) {
     if (this.password == null &&
         (this.lmPassword == null || this.ntPassword == null)) {
@@ -26,16 +34,19 @@ class NTLMClient {
         "You must provide a password or the LM and NT hash of a password.",
       );
     }
+
+    this._inner = inner ?? Client();
   }
 
-  Future<Response> get(String url, {Map<String, String> headers}) async {
+  Future<Response> _ntlm({
+    Map<String, String> headers,
+    _RequestCallback request,
+  }) async {
     if (headers == null) {
-      headers = new Map<String, String>();
+      headers = Map<String, String>();
     }
 
-    var client = Client();
-
-    Response res0 = await client.get(url, headers: headers);
+    Response res0 = await request(headers);
     if (res0.statusCode == 200 ||
         res0.headers[HttpHeaders.wwwAuthenticateHeader] != "NTLM") return res0;
 
@@ -43,10 +54,9 @@ class NTLMClient {
       domain: domain,
       workstation: workstation,
     );
-    Response res2 = await client.get(url,
-        headers: {
-          HttpHeaders.authorizationHeader: msg1,
-        }..addAll(headers));
+    Response res2 = await request({
+      HttpHeaders.authorizationHeader: msg1,
+    }..addAll(headers));
 
     String res2Authenticate = res2.headers[HttpHeaders.wwwAuthenticateHeader];
     if (!res2Authenticate.startsWith("NTLM ")) return res0;
@@ -61,13 +71,79 @@ class NTLMClient {
       lmPassword: lmPassword,
       ntPassword: ntPassword,
     );
-    Response res3 = await client.get(url,
-        headers: {
-          HttpHeaders.authorizationHeader: msg3,
-        }..addAll(headers));
-
-    client.close();
+    Response res3 = await request({
+      HttpHeaders.authorizationHeader: msg3,
+    }..addAll(headers));
 
     return res3;
+  }
+
+  Future<Response> get(url, {Map<String, String> headers}) async {
+    return _ntlm(
+      headers: headers,
+      request: (ntlmHeaders) => _inner.get(
+            url,
+            headers: ntlmHeaders,
+          ),
+    );
+  }
+
+  Future<Response> post(url,
+      {Map<String, String> headers, body, Encoding encoding}) {
+    return _ntlm(
+      headers: headers,
+      request: (ntlmHeaders) => _inner.post(
+            url,
+            headers: ntlmHeaders,
+            body: body,
+            encoding: encoding,
+          ),
+    );
+  }
+
+  Future<Response> patch(url,
+      {Map<String, String> headers, body, Encoding encoding}) {
+    return _ntlm(
+      headers: headers,
+      request: (ntlmHeaders) => _inner.patch(
+            url,
+            headers: ntlmHeaders,
+            body: body,
+            encoding: encoding,
+          ),
+    );
+  }
+
+  Future<Response> put(url,
+      {Map<String, String> headers, body, Encoding encoding}) {
+    return _ntlm(
+      headers: headers,
+      request: (ntlmHeaders) => _inner.put(
+            url,
+            headers: ntlmHeaders,
+            body: body,
+            encoding: encoding,
+          ),
+    );
+  }
+
+  Future<Response> head(url, {Map<String, String> headers}) {
+    return _ntlm(
+      headers: headers,
+      request: (ntlmHeaders) => _inner.head(
+            url,
+            headers: ntlmHeaders,
+          ),
+    );
+  }
+
+  Future<Response> delete(url, {Map<String, String> headers}) {
+    return _ntlm(
+      headers: headers,
+      request: (ntlmHeaders) => _inner.delete(
+            url,
+            headers: ntlmHeaders,
+          ),
+    );
   }
 }
